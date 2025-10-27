80b317c237d3aaa4f3058911f749f4efd9be347a
jsobbisp <jakub.sobocinski@bilfinger.com>
2025-10-27 15:07:09 +0100

    Update sw_register.js

--- sw_register.js
+++ sw_register.js
@@ -1,5 +1,1 @@
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
-    .then(reg=>console.log('Service Worker zarejestrowany', reg))
-    .catch(err=>console.log('Błąd rejestracji SW',err));
-}
+
